697acc17039e3ea4921acc7c60a8a385d0009642
LudaKend <Ludakend@gmail.com>
2023-08-20 22:55:29 +0300

    версия 1

---
+++ tests/test_utils.py
@@ -0,0 +1,17 @@
+def test_load_operations():
+    pass
+#    assert load_operations([1, 2, 3], 1, "test") == 2
+
+def take_only_executed(operations):
+    pass
+
+
+def get_last_operations(operations_executed):
+    pass
+
+
+def transformation_date(last_operations):
+    pass
+
+def printout(last_operations_date):
+    pass
